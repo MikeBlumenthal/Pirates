17291c727cb5887ea472939894c4751f1a88dfc6
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-23 14:21:34 +0100

    init commit with prior code

---
+++ src/main/java/com/codeclan/example/pirateservice_d1_starter/repositories/PirateRepository.java
@@ -0,0 +1,10 @@
+package com.codeclan.example.pirateservice_d1_starter.repositories;
+
+import com.codeclan.example.pirateservice_d1_starter.models.Pirate;
+import com.codeclan.example.pirateservice_d1_starter.projections.Embed;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(excerptProjection = Embed.class)
+public interface PirateRepository extends JpaRepository<Pirate, Long> {
+}
---
+++ src/test/java/com/codeclan/example/pirateservice_d1_starter/PirateserviceD1StarterApplicationTests.java
@@ -0,0 +1,24 @@
+package com.codeclan.example.pirateservice_d1_starter;
+
+import com.codeclan.example.pirateservice_d1_starter.models.Pirate;
+import com.codeclan.example.pirateservice_d1_starter.models.Raid;
+import com.codeclan.example.pirateservice_d1_starter.models.Ship;
+import com.codeclan.example.pirateservice_d1_starter.repositories.PirateRepository;
+import com.codeclan.example.pirateservice_d1_starter.repositories.RaidRepository;
+import com.codeclan.example.pirateservice_d1_starter.repositories.ShipRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PirateserviceD1StarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+
+}
